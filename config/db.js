0cbac14aa028fdb18f3bbb719799ed996c64171e
MahmoudFadl93 <38670015+mahmoudfadl93@users.noreply.github.com>
2020-10-24 16:51:45 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('config');// you have to make file and connect to your data base
+const config = require('config');// you have to make json file and connect to your data base
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
